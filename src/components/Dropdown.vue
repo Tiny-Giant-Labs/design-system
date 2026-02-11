f3292ae8545f39972d815bf679906e02dd7621d1
volkan-elci <velcidevelopment@gmail.com>
2026-02-11 17:10:43 +0300

    dropdown style changes

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -135,3 +135,15 @@ onBeforeUnmount(() => {
     </div>
   </div>
 </template>
+
+<style>
+.tippy-box[data-theme~="transparent"] {
+  background-color: transparent !important;
+  box-shadow: none !important;
+  z-index: 10001;
+}
+.tippy-box[data-theme~="transparent"] .tippy-content {
+  padding: 0;
+  color: #0a0a0a;
+}
+</style>
